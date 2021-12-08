a831da985560b7df31e475a11fc4c7eed970ce8e
Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-12-08 14:16:54 +0100

    test: added product controller test file

---
+++ tests/Host.IntegrationTests/ControllersTests/Catalog/ProductsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Host.IntegrationTests.Fixtures;
+using Shouldly;
+
+namespace Host.IntegrationTests.ControllersTests.Catalog;
+
+public class ProductsControllerTests : BaseControllerTests
+{
+    public ProductsControllerTests(CustomWebApplicationFactory<Program> fixture)
+        : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task GetAsync_Without_Login_Should_Throw_Not_Authorized()
+    {
+        // Arrange
+        var productId = "623e0000-3f5a-3c7c-0502-08d9b2523534";
+
+        var result = await _client.GetAsync($"/v1/products/{productId}");
+
+        result.StatusCode.ShouldBe(System.Net.HttpStatusCode.Unauthorized);
+    }
+}
