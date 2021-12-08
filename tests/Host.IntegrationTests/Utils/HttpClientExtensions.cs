f1956b8c2b6680c0cb4c07386b63c8afe0f06e72
Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-12-08 14:16:52 +0100

    test: added httpClient extension

---
+++ tests/Host.IntegrationTests/Utils/HttpClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DN.WebApi.Infrastructure.Identity.Models;
+using System.Net.Http.Headers;
+using static Host.IntegrationTests.Mocks.TokenServiceMock;
+
+namespace Host.IntegrationTests.Utils;
+
+public static class HttpClientExtensions
+{
+    public static HttpClient SetFakeBearerToken(this HttpClient client, ApplicationUser user)
+    {
+        client.DefaultRequestHeaders.Authorization = (AuthenticationHeaderValue?)new AuthenticationHeaderValue("Bearer", GenerateJwtToken(GetClaims(user, "locahost")));
+        return client;
+    }
+}
