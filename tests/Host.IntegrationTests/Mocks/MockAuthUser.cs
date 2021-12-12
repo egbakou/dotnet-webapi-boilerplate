8f1400f426a06033d91b7692a107b50cd75a0071
Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-12-12 08:31:38 +0100

    refactor: added mocked jwt scheme, auth, hander and scheme provider

---
+++ tests/Host.IntegrationTests/Mocks/MockAuthUser.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Host.IntegrationTests.Mocks;
+
+public class MockAuthUser
+{
+    public List<Claim> Claims { get; private set; } = new();
+
+    public MockAuthUser(params Claim[] claims)
+        => Claims = claims.ToList();
+}
---
+++ tests/Host.IntegrationTests/Mocks/MockSchemeProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace Host.IntegrationTests.Mocks;
+
+public class MockSchemeProvider : AuthenticationSchemeProvider
+{
+    public MockSchemeProvider(IOptions<AuthenticationOptions> options)
+        : base(options)
+    {
+    }
+
+    protected MockSchemeProvider(
+        IOptions<AuthenticationOptions> options,
+        IDictionary<string, AuthenticationScheme> schemes
+    )
+        : base(options, schemes)
+    {
+    }
+
+    public override Task<AuthenticationScheme?> GetSchemeAsync(string name)
+    {
+        if (name == FakeJwtBearerDefaults.AuthenticationScheme)
+        {
+            var scheme = new AuthenticationScheme(
+                FakeJwtBearerDefaults.AuthenticationScheme,
+                FakeJwtBearerDefaults.AuthenticationScheme,
+                typeof(MockAuthenticationHandler)
+            );
+            return Task.FromResult(scheme);
+        }
+
+        return base.GetSchemeAsync(name);
+    }
+}
