c00bd8f2e0330aa02411a332b8bb15d9ae882aed
Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2021-12-12 08:30:04 +0100

    refactor: db bootstapper utils updated

--- tests/Host.IntegrationTests/Utils/DbBootstrapperUtils.cs
+++ tests/Host.IntegrationTests/Utils/DbBootstrapperUtils.cs
@@ -1,30 +1,59 @@
 ﻿using DN.WebApi.Domain.Catalog;
 using DN.WebApi.Infrastructure.Persistence.Contexts;
 using System.Reflection;
-using System.Security.Claims;
-using System.Text.Json;
 using DN.WebApi.Domain.Constants;
 using DN.WebApi.Domain.Multitenancy;
 using DN.WebApi.Infrastructure.Common.Extensions;
 using DN.WebApi.Infrastructure.Identity.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Host.IntegrationTests.Fixtures;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Security.Claims;
+using Newtonsoft.Json;
 
 namespace Host.IntegrationTests.Utils;
 
 public static class DbBootstrapperUtils
 {
+
+    public static void CreateDbAndSeedDataIfNotExists(IServiceProvider scopedServices)
+    {
+        var logger = scopedServices
+            .GetRequiredService<ILogger<CustomWebApplicationFactory<Program>>>();
+        try
+        {
+            var tenantDbContext = scopedServices.GetRequiredService<TenantManagementDbContext>();
+
+            SeedRootTenant(tenantDbContext);
+
+            var appDbContext = scopedServices.GetRequiredService<ApplicationDbContext>();
+            var userManager = scopedServices.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = scopedServices.GetRequiredService<RoleManager<ApplicationRole>>();
+            SeedTenantDatabase(appDbContext, tenantDbContext, userManager, roleManager);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Error creating tables and seed data: {ex.Message}");
+        }
+    }
+
     public static void SeedRootTenant(TenantManagementDbContext dbContext)
     {
-        var rootTenant = new Tenant(
-            MultitenancyConstants.Root.Name,
-            MultitenancyConstants.Root.Key,
-            MultitenancyConstants.Root.EmailAddress,
-            "InMemoryConnectionString");
-        rootTenant.SetValidity(DateTime.UtcNow.AddYears(1));
-        dbContext.Tenants.Add(rootTenant);
-        dbContext.SaveChanges();
+        if (!dbContext.Tenants.Any(t => t.Key == MultitenancyConstants.Root.Key))
+        {
+            var rootTenant = new Tenant(
+                MultitenancyConstants.Root.Name,
+                MultitenancyConstants.Root.Key,
+                MultitenancyConstants.Root.EmailAddress,
+                "TenantDbForTesting");
+            rootTenant.SetValidity(DateTime.UtcNow.AddYears(1));
+            dbContext.Tenants.Add(rootTenant);
+            dbContext.SaveChanges();
+        }
     }
 
     public static void SeedTenantDatabase(
@@ -34,29 +63,28 @@ public static class DbBootstrapperUtils
         RoleManager<ApplicationRole> roleManager)
     {
         var tenant = tenantContext.Tenants.First();
-        SeedRolesAsync(tenant, roleManager, appDbContext).GetAwaiter().GetResult();
-        SeedTenantAdminAsync(tenant, userManager, roleManager, appDbContext).GetAwaiter().GetResult();
+        SeedRolesAsync(tenant, appDbContext, roleManager).GetAwaiter().GetResult();
+        SeedTenantAdminAsync(tenant, appDbContext, userManager, roleManager).GetAwaiter().GetResult();
         SeedCatalog(appDbContext, tenant);
     }
 
-    #region Identity Seed Data
-    private static async Task SeedRolesAsync(Tenant tenant, RoleManager<ApplicationRole> roleManager, ApplicationDbContext applicationDbContext)
+    private static async Task SeedRolesAsync(Tenant tenant, ApplicationDbContext dbContext, RoleManager<ApplicationRole> roleManager)
     {
         var roles = typeof(RoleConstants).GetAllPublicConstantValues<string>();
 
         foreach (string roleName in roles)
         {
-            var roleStore = new RoleStore<ApplicationRole>(applicationDbContext);
+            var roleStore = new RoleStore<ApplicationRole>(dbContext);
 
             var role = new ApplicationRole(roleName, tenant.Key, $"{roleName} Role for {tenant.Key} Tenant");
-            if (!await applicationDbContext.Roles.IgnoreQueryFilters().AnyAsync(r => r.Name == roleName && r.Tenant == tenant.Key))
+            if (!await dbContext.Roles.IgnoreQueryFilters().AnyAsync(r => r.Name == roleName && r.Tenant == tenant.Key))
             {
                 await roleStore.CreateAsync(role);
             }
 
             if (roleName == RoleConstants.Basic)
             {
-                var basicRole = await roleManager.Roles.IgnoreQueryFilters()
+                var basicRole = await dbContext.Roles.IgnoreQueryFilters()
                     .Where(a => a.NormalizedName == RoleConstants.Basic.ToUpper() && a.Tenant == tenant.Key)
                     .FirstOrDefaultAsync();
                 if (basicRole is null)
@@ -73,7 +101,7 @@ public static class DbBootstrapperUtils
         }
     }
 
-    private static async Task SeedTenantAdminAsync(Tenant tenant, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, ApplicationDbContext applicationDbContext)
+    private static async Task SeedTenantAdminAsync(Tenant tenant, ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
     {
         if (string.IsNullOrEmpty(tenant.Key) || string.IsNullOrEmpty(tenant.AdminEmail))
         {
@@ -94,31 +122,32 @@ public static class DbBootstrapperUtils
             IsActive = true,
             Tenant = tenant.Key.Trim().ToLower()
         };
-        if (!applicationDbContext.Users.IgnoreQueryFilters().Any(u => u.Email == tenant.AdminEmail))
+        superUser.Id = "43d7aab7-4136-4842-9a78-bb13c5ad49bc";
+        if (!dbContext.Users.IgnoreQueryFilters().Any(u => u.Email == tenant.AdminEmail))
         {
             var password = new PasswordHasher<ApplicationUser>();
             superUser.PasswordHash = password.HashPassword(superUser, MultitenancyConstants.DefaultPassword);
-            var userStore = new UserStore<ApplicationUser>(applicationDbContext);
+            var userStore = new UserStore<ApplicationUser>(dbContext);
             await userStore.CreateAsync(superUser);
         }
 
-        await AssignAdminRoleAsync(superUser.Email, tenant.Key, applicationDbContext, userManager, roleManager);
+        await AssignAdminRoleAsync(superUser.Email, tenant.Key, dbContext, userManager, roleManager);
     }
 
-    private static async Task AssignAdminRoleAsync(string email, string tenant, ApplicationDbContext applicationDbContext, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+    private static async Task AssignAdminRoleAsync(string email, string tenant, ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
     {
         var user = await userManager.Users.IgnoreQueryFilters()
-            .FirstOrDefaultAsync(u => u.Email.Equals(email));
+             .FirstOrDefaultAsync(u => u.Email.Equals(email));
         if (user == null) return;
         var roleRecord = await roleManager.Roles.IgnoreQueryFilters()
             .Where(a => a.NormalizedName == RoleConstants.Admin.ToUpper() && a.Tenant == tenant)
             .FirstOrDefaultAsync();
         if (roleRecord == null) return;
-        bool isUserInRole = await applicationDbContext.UserRoles.AnyAsync(a => a.UserId == user.Id && a.RoleId == roleRecord.Id);
+        bool isUserInRole = await dbContext.UserRoles.AnyAsync(a => a.UserId == user.Id && a.RoleId == roleRecord.Id);
         if (!isUserInRole)
         {
-            applicationDbContext.UserRoles.Add(new IdentityUserRole<string>() { RoleId = roleRecord.Id, UserId = user.Id });
-            await applicationDbContext.SaveChangesAsync();
+            dbContext.UserRoles.Add(new IdentityUserRole<string>() { RoleId = roleRecord.Id, UserId = user.Id });
+            await dbContext.SaveChangesAsync();
         }
 
         var allClaims = await roleManager.GetClaimsAsync(roleRecord);
@@ -141,11 +170,13 @@ public static class DbBootstrapperUtils
             }
         }
 
-        await applicationDbContext.SaveChangesAsync();
-    }
-    #endregion
+        await dbContext.SaveChangesAsync();
 
-    #region Catalog Seed Data
+        var allroles = userManager.Users.IgnoreQueryFilters().ToList();
+        foreach (var r in allroles)
+            Debug.WriteLine(r.Id);
+    }
+
     private static void SeedCatalog(ApplicationDbContext db, Tenant tenant)
     {
         db.Tenant = tenant.Key;
@@ -167,15 +198,16 @@ public static class DbBootstrapperUtils
     private static List<Brand>? GetBrandsSeeding()
     {
         string? path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        string brandsData = File.ReadAllText(path + "/Utils/brands.json");
-        return JsonSerializer.Deserialize<List<Brand>>(brandsData);
+        string brandsData = File.ReadAllText(path + "/Seed/brands.json");
+        var data = JsonConvert.DeserializeObject<List<Brand>>(brandsData);
+        return data;
     }
 
     private static List<Product>? GetProductsSeeding()
     {
         string? path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        string productsData = File.ReadAllText(path + "/Utils/products.json");
-        return JsonSerializer.Deserialize<List<Product>>(productsData);
+        string productsData = File.ReadAllText(path + "/Seed/products.json");
+        var data = JsonConvert.DeserializeObject<List<Product>>(productsData);
+        return data;
     }
-    #endregion
 }
